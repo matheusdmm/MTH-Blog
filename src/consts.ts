826a8555232a7ed740b6a4b4596e6886081f2234
Matheus <matheusdmm@users.noreply.github.com>
2026-05-10 18:38:28 -0300

    kofi link changed to mercado pago

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@ export const SITE_TITLE = 'MATHEUSDMM';
 export const SITE_DESCRIPTION = 'My place, be welcome!';
 export const GITHUB = 'https://github.com/matheusdmm';
 export const BANDCAMP = 'https://aheroforghosts.bandcamp.com';
-export const KOFI = 'https://ko-fi.com/'; // TODO
+export const KOFI = 'link.mercadopago.com.br/matheusdmm'; // TODO
