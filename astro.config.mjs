474e001551368fd63e3a329b6926edcc341ea026
Matheus <matheusdmm@users.noreply.github.com>
2026-06-15 19:04:24 -0300

    theme revamp, light theme is noprd light and dark theme is ayu

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
-
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import { defineConfig, fontProviders } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://www.matheusdmm.dev/",
   integrations: [mdx(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
